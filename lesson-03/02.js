bfd18ad65946197a6d2f8dc41861fd3ace39cc5d
v_klevchuk <lemoniklava@gmail.com>
2023-09-04 13:57:44 +0800

    orange true

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,5 +1,5 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-if (words.indexOf("апельсин")) {
+if (words.indexOf("апельсин") == true) {
     console.log("Ура! нашел");
 } else {
     console.log("Придется поискать в другом магазине…");
